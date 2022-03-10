19a1d27859dc85a1134c9dfed95470a6dea95884
PatriciaPala <101339915+PatriciaPala@users.noreply.github.com>
2022-03-10 09:28:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onPinPressed(TouchPin.P0, function () {
+    music.playMelody("G D A C E G D - ", 120)
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
